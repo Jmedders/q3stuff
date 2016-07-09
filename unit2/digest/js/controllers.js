67bebc35e10beea74ad38e07aa764ee36275f2d6
Jeff <medders.jeffrey@gmail.com>
2016-07-08 22:53:55 -0600

    added digest code

---
+++ unit2/digest/js/controllers.js
@@ -0,0 +1,16 @@
+function MainController($rootScope, $scope) {
+  $rootScope.rootView = {};
+  $rootScope.rootView.name = "Fido";
+  $scope.view = {};
+  $scope.view.age = 3;
+
+  // this is for example purposes
+  // NOTE - there is a $timeout which handles $apply for you
+  setTimeout(function() {
+    $rootScope.rootView.name = "Lassie";
+    $scope.view.age = 10;
+    // $scope.$digest();
+    $scope.$apply();
+  }, 1000);
+}
+app.controller("MainController", MainController)
