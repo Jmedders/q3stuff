85373fc255b6eedbbbd9fc9fbf2fc45ed1d0f653
Jeff <medders.jeffrey@gmail.com>
2016-07-13 13:43:27 -0600

    sort functionality working

--- unit2/moviesearchapp/controllers.js
+++ unit2/moviesearchapp/controllers.js
@@ -1,22 +1,46 @@
 app.controller('searchController', function($scope, $rootScope, $http, $routeParams){
   $scope.view = {};
   if($rootScope.roots){
-    $http.get('http://www.omdbapi.com/?s=' + $rootScope.roots.searchingOMDB).then(function(data){
-      $scope.view.searchresult = data['data']['Search'];
-    });
-    $scope.view.searching = function(){
-      $http.get('http://www.omdbapi.com/?s=' + $scope.view.searchOMDB).then(function(data){
-          $scope.view.searchresult = data['data']['Search'];
+    // console.log('before search function in root scope', $rootScope.roots.selector);
+
+    // console.log('im in rootscope searching where rootScope: ', $rootScope.roots.searchingOMDB);
+    if($rootScope.roots.selector){
+      console.log('line 9: ', $rootScope.roots.selector);
+      $http.get('http://www.omdbapi.com/?s=' + $rootScope.roots.searchingOMDB + '&type=' + $rootScope.roots.selector).then(function(data){
+        $scope.view.searchresult = data['data']['Search'];
       });
+    } else{
+      $http.get('http://www.omdbapi.com/?s=' + $rootScope.roots.searchingOMDB).then(function(data){
+        $scope.view.searchresult = data['data']['Search'];
+      });
+    }
+    $scope.view.searching = function(){
+
+      if($rootScope.roots.selector){
+        $rootScope.roots.selector = $scope.view.selector
+        console.log('line 20: ',  $rootScope.roots.selector);
+        $http.get('http://www.omdbapi.com/?s=' + $scope.view.searchOMDB + '&type=' + $rootScope.roots.selector).then(function(data){
+          $scope.view.searchresult = data['data']['Search'];
+        });
+      } else {
+        $http.get('http://www.omdbapi.com/?s=' + $scope.view.searchOMDB).then(function(data){
+          $scope.view.searchresult = data['data']['Search'];
+        });
+      }
     }
   } else if($scope.view){
     // no rootscope
     $scope.view.searching = function(){
-      console.log('hi im searching');
-      console.log('regularscope: ', $scope.view.searchOMDB);
-      $http.get('http://www.omdbapi.com/?s=' + $scope.view.searchOMDB).then(function(data){
-        $scope.view.searchresult = data['data']['Search'];
-      });
+      if($scope.view.selector){
+        console.log('line 35', $scope.view.selector);
+        $http.get('http://www.omdbapi.com/?s=' + $scope.view.searchOMDB + '&type=' + $scope.view.selector).then(function(data){
+          $scope.view.searchresult = data['data']['Search'];
+        });
+      } else{
+          $http.get('http://www.omdbapi.com/?s=' + $scope.view.searchOMDB).then(function(data){
+            $scope.view.searchresult = data['data']['Search'];
+          });
+      }
     }
   }
 })
@@ -29,11 +53,13 @@ app.controller('resultController', function($scope, $rootScope, $http, $routePar
     $scope.view.poster = data['data']['Poster'];
     $scope.view.actors = data['data']['Actors'];
     $scope.view.director = data['data']['Director'];
+    $scope.view.plot = data['data']['Plot'];
   });
   $scope.view.go = function (path) {
     $location.path(path);
   };
   $rootScope.roots.searches = function(){
     $rootScope.roots.searchingOMDB;
+    $rootScope.roots.selector = $scope.view.selector;
   };
 })
