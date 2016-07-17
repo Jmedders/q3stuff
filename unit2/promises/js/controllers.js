4adebb02f6b2b3b4899c51ca873f1caa84e29ed9
Jeff <medders.jeffrey@gmail.com>
2016-07-17 14:53:34 -0600

    working with promises

--- unit2/promises/js/controllers.js
+++ unit2/promises/js/controllers.js
@@ -1,7 +1,10 @@
 app.controller('promiseController', function($scope, promiseService){
   $scope.view = {};
   $scope.view.lookPromise = function(){
-    $scope.view.promiseboy = promiseService.catchEm($scope.view.inputs)
-    console.log($scope.view.promiseboy);
+    console.log('hi there');
+    promiseService.catchEm($scope.view)
+    // console.log($scope.view.promiseboy);
+    // console.log(pokeName);
   }
+  console.log('anything');
 })
--- unit2/promises/js/services.js
+++ unit2/promises/js/services.js
@@ -5,14 +5,18 @@ app.service("promiseService", function($http) {
   // A number for the pokemon id needs to be added to the
   // this path.
   return {
-    this.pokemonName 
-    catchEm: function(pokemonId) {
+    catchEm: function(view) {
+      console.log(view.inputs);
+      var pokemonId = view.inputs
       var pokemonInfoPath = 'api/v1/pokemon/';
       var pokeData;
       var fullUrl = baseUrl + pokemonInfoPath + pokemonId + '/';
-      $http.get(fullUrl).then(function(baseData) {
-        pokemonName = baseData.data['name'];
-      })
+      $http.get(fullUrl).then(function(data) {
+        pokemonName = data.data['name'];
+        console.log(pokemonName);
+        view.promiseboy = pokemonName;
+      });
+      return 'junk'
     }
   }
 });
